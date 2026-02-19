3ac61b47400da9ee4c61e422f7f26cbfa15bed03
Danny <179722984+DanielProano@users.noreply.github.com>
2026-02-19 16:54:59 -0500

    Permissions

--- frontend/server.js
+++ frontend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get((req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
 
